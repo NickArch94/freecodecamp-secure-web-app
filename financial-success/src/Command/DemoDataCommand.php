e38e530be54178b9993f33ad19c931eeb67f40cd
NickArch94 <niok123messages@gmail.com>
2026-06-11 09:32:23 -0600

    Finished configuring the Demo Data document, but am having errors populating the demo data.

--- financial-success/src/Command/DemoDataCommand.php
+++ financial-success/src/Command/DemoDataCommand.php
@@ -37,7 +37,7 @@ class DemoDataCommand
 
     private function createAccounts(SymfonyStyle $io): void
     {
-        $io->session(message: 'Creating demo accounts...');
+        $io->section(message: 'Creating demo accounts...');
 
         $accounts = [
             ['Jonathan Doe', '1234567890', 50000.00, '123-45-6789', 'jonathan.doe@example.com'],
@@ -52,29 +52,24 @@ class DemoDataCommand
         }
     }
 
-    protected function configure(): void
+    private function createTransactions(SymfonyStyle $io): void
     {
-        $this
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
-        ;
-    }
+        $io->session(message: 'Creating demo transactions...');
 
-    protected function execute(InputInterface $input, OutputInterface $output): int
-    {
-        $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
+        $transactions = [
+            ['1234567890', 1500.00, 'deposit', 'Initial deposit', '4111111111111111', '123', '12/25', 'Amazon', new \DateTime('2024-01-15')],
+            ['1234567890', 200.00, 'withdrawal', 'ATM withdrawal', '4111111111111111', '123', '12/25', 'ATM Withdrawal', new \DateTime('2024-02-10')],
+            ['8675309876', 5000.00, 'deposit', 'Paycheck deposit', '5555555555554444', '456', '11/24', 'Employer Inc.', new \DateTime('2024-01-30')],
+            ['8675309876', 300.00, 'withdrawal', 'Grocery shopping', '5555555555554444', '456', '11/24', 'Whole Foods Market', new \DateTime('2024-02-05')],
+            ['5551234567', 10000.00, 'deposit', 'Freelance payment', '378282246310005', '789', '10/23', 'Client LLC', new \DateTime('2024-01-20')],
+            ['5551234567', 1500.00, 'withdrawal', 'Online shopping spree', '378282246310005', '789', '10/23', 'Best Buy Online Store', new \DateTime('2024-02-12')],
+            ['4449876543', 2500.00, 'deposit', "Robin's paycheck", '6011111111111117', '321', '09/26', "Robin's Employer", new \DateTime('2024-01-25')],
+            ['4449876543', 500.00, 'withdrawal', "Robin's dinner out", '6011111111111117', '321', '09/26', "Fancy Restaurant", new \DateTime('2024-02-14')]
+        ];
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
+        foreach ($transactions as [$accountNumber, $amount, $type, $description, $cardNumber, $cvv, $expiryDate, $merchantName, $transactionDate]) {
+            $transaction = $this->financialService->createTransaction(accountNumber: $accountNumber, amount: $amount, transactionType: $type, description: $description, cardNumber: $cardNumber, cvv: $cvv, expiryDate: $expiryDate, merchantName: $merchantName, transactionDate: $transactionDate);
+            $io->text(message: "Created {$type} transaction of \${$amount} for account {$accountNumber} (ID: {$transaction->getId()})");
         }
-
-        if ($input->getOption('option1')) {
-            // ...
-        }
-
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
-
-        return Command::SUCCESS;
     }
 }
